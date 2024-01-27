48ce406902e2a83258b029b0b0a1fc75f0d90928
Shyam Makwana <makwanashyam6@gmail.com>
2024-01-27 17:22:01 -0500

    Change for all the tabs in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ function Header() {
     const auth = useContext(AuthContext);
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="/"><span style={{color: "white", fontWeight: "bold"}}>FoodSaver</span></Navbar.Brand>
+            <Navbar.Brand href="/"><span style={{ color: "red", fontWeight: "bold" }}>FoodSaver</span></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/home"><span style={{color: "white"}}>Home</span></Nav.Link>
-                    {auth.isLoggedIn && (<Nav.Link href="/donate"><span style={{color: "white"}}>Donate</span></Nav.Link>)}
-                    {auth.isLoggedIn && (<Nav.Link><Link to="/reqforfood">Request For Food</Link></Nav.Link>)}
-                    <Nav.Link><Link to="/contributors">Contributors</Link></Nav.Link>
-                    <Nav.Link><Link to="/contact">Contact Us</Link></Nav.Link>
+                    <Nav.Link href="/home"><span style={{ color: "white" }}>Home</span></Nav.Link>
+                    {auth.isLoggedIn && (<Nav.Link href="/donate"><span style={{ color: "white" }}>Donate</span></Nav.Link>)}
+                    {auth.isLoggedIn && (<Nav.Link href="/reqforfood"><span style={{ color: "white" }}>Request For Food</span></Nav.Link>)}
+                    <Nav.Link href="/contributors"><span style={{ color: "white" }}>Contributors</span></Nav.Link>
+                    <Nav.Link href="/contact"><span style={{ color: "white" }}>Contact Us</span></Nav.Link>
                 </Nav>
                 <Nav >
                     {!auth.isLoggedIn && (<Link to="/login"><Button variant="danger">Login</Button></Link>)}
@@ -31,7 +31,7 @@ function Header() {
                         </Dropdown.Menu>
                     </Dropdown>}
                     {auth.isLoggedIn && (
-                        <a><Button variant="danger" onClick={auth.logout}>Logout</Button></a>
+                        <Button variant="danger" onClick={auth.logout}>Logout</Button>
                     )}
                 </Nav>
             </Navbar.Collapse>
