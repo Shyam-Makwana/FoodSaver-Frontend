e06ee10407be00795afd4756da035bbb1dfeaa9c
Shyam Makwana <makwanashyam6@gmail.com>
2024-01-27 17:14:58 -0500

    Change url for 2 pages in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ function Header() {
     const auth = useContext(AuthContext);
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Link to="/"><Navbar.Brand>FoodSaver</Navbar.Brand></Link>
+            <Navbar.Brand href="/"><span style={{color: "white", fontWeight: "bold"}}>FoodSaver</span></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Link to="/home"><Nav.Link>Home</Nav.Link></Link>
-                    {auth.isLoggedIn && (<Link to="/donate"><Nav.Link>Donate</Nav.Link></Link>)}
-                    {auth.isLoggedIn && (<Link to="/reqforfood"><Nav.Link>Request For Food</Nav.Link></Link>)}
-                    <Link to="/contributors"><Nav.Link>Contributors</Nav.Link></Link>
-                    <Link to="/contact"><Nav.Link>Contact Us</Nav.Link></Link>
+                    <Nav.Link href="/home"><span style={{color: "white"}}>Home</span></Nav.Link>
+                    {auth.isLoggedIn && (<Nav.Link href="/donate"><span style={{color: "white"}}>Donate</span></Nav.Link>)}
+                    {auth.isLoggedIn && (<Nav.Link><Link to="/reqforfood">Request For Food</Link></Nav.Link>)}
+                    <Nav.Link><Link to="/contributors">Contributors</Link></Nav.Link>
+                    <Nav.Link><Link to="/contact">Contact Us</Link></Nav.Link>
                 </Nav>
                 <Nav >
                     {!auth.isLoggedIn && (<Link to="/login"><Button variant="danger">Login</Button></Link>)}
@@ -25,13 +25,13 @@ function Header() {
                             Profile
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item><Link to="/viewprofile">View Profile</Link></Dropdown.Item>
+                            <Dropdown.Item ><Link to="/viewprofile">View Profile</Link></Dropdown.Item>
                             <Dropdown.Item><Link to="/userdonfood">Donated Food</Link></Dropdown.Item>
                             <Dropdown.Item><Link to="/userrecfood">Received Food</Link></Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>}
                     {auth.isLoggedIn && (
-                        <Button variant="danger" onClick={auth.logout}>Logout</Button>
+                        <a><Button variant="danger" onClick={auth.logout}>Logout</Button></a>
                     )}
                 </Nav>
             </Navbar.Collapse>
